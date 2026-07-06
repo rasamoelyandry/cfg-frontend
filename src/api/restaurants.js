a1149d21a3e1c97058d4f64f950a1c38b409fc9b
rasamoelyandry <rasamoelyandrystephane@gmail.com>
2026-07-06 10:17:22 +0200

    Initial commit: cfg-frontend

---
+++ src/api/restaurants.js
@@ -0,0 +1,27 @@
+import client from './client.js';
+
+export const restaurantsApi = {
+  list: async () => {
+    const res = await client.get('/restaurants');
+    return res.data.data;
+  },
+
+  get: async (id) => {
+    const res = await client.get(`/restaurants/${id}`);
+    return res.data.data;
+  },
+
+  create: async (data) => {
+    const res = await client.post('/restaurants', data);
+    return res.data.data;
+  },
+
+  update: async (id, data) => {
+    const res = await client.put(`/restaurants/${id}`, data);
+    return res.data.data;
+  },
+
+  delete: async (id) => {
+    await client.delete(`/restaurants/${id}`);
+  },
+};
